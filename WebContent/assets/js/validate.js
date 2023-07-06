2cbe0ce172e1dd140ac2c1facad56d1b156a8777
Bcienz3 <127243887+Bcienz3@users.noreply.github.com>
2023-07-06 17:44:56 +0200

    Delete WebContent directory
    cancello perchè la metto nella posizione corretta

--- WebContent/assets/js/validate.js
+++
@@ -1,8 +0,0 @@
-function validateField(elem, errorElem, errorMsg){
-	if(!elem.checkValidity()){
-		errorElem.textContent = errorMsg;
-		errorElem.style.display = "flex";
-	}else{
-		errorElem.style.display = "none";
-	}
-}
